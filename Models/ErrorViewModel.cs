5b99fac3e59f9af0f4bed2bbdbfbcca71de0cd46
daniele.missio <danielemissio0907@gmail.com>
2019-09-17 18:55:53 -0300

    First commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace telas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

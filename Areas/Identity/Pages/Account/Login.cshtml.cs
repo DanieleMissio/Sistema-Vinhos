f1dce66a3c6a8b7bec726c95efd45a73ac54b227
daniele.missio <danielemissio0907@gmail.com>
2019-09-22 10:17:29 -0300

    Atualizado

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,7 +44,7 @@ namespace telas.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembre-me?")]
             public bool RememberMe { get; set; }
         }
 
